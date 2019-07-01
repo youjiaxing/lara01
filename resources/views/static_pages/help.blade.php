4795ae44d5b3296bd4955e89df7f02fbe20d9735
youjiaxing <287009007@qq.com>
2019-07-01 01:40:08 +0000

    Finish static pages

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,7 @@
+@extends("layouts.default")
+
+@section("title", "帮助")
+
+@section("content")
+    <h1>HELP</h1>
+    @stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends("layouts.default")
+
+@section("title", "主页")
+
+@section("content")
+    <h1>HOME</h1>
+    @stop
