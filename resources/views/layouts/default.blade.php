698b1de4d5068e53ebcac4384d6dc5679f205182
youjiaxing <287009007@qq.com>
2019-07-01 06:00:49 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,12 +1,21 @@
-<!doctype html>
-<html lang="zh-CN">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield("title", config('app.name')) - Lara01教程</title>
-</head>
-<body>
-@yield("content")
-</body>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include("layouts._header")
+
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+
+        @include("layouts._footer")
+      </div>
+
+    </div>
+
+
+  </body>
 </html>
